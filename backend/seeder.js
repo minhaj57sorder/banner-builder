ef3c34591815b438553c44ac693b46fdba081b51
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-04-15 20:22:42 +0600

    updated fix

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import users from './data/users.js'
-import products from './data/products.js'
 import User from './models/userModel.js'
-import Product from './models/productModel.js'
-import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -13,19 +10,12 @@ connectDB()
 
 const importData = async () => {
     try {
-        await Order.deleteMany()
-        await Product.deleteMany()
         await User.deleteMany()
 
         const createdUsers = await User.insertMany(users)
 
         const adminUser = createdUsers[0]._id
 
-        const sampleProducts = products.map((product) => {
-            return { ...product, user: adminUser }
-        })
-
-        await Product.insertMany(sampleProducts)
 
         console.log('Data Imported!')
         process.exit()
@@ -37,8 +27,6 @@ const importData = async () => {
 
 const destroyData = async () => {
     try {
-        await Order.deleteMany()
-        await Product.deleteMany()
         await User.deleteMany()
 
         console.log('Data Destroyed!')
